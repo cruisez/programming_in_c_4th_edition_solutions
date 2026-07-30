12c24766aa04cd42af6567eca4b550b771684ef5
Soorya <sooryaghanesh@gmail.com>
2026-07-30 12:53:45 +0000

    Just saving my progress

--- Chapter_3/Prog/prog3.3.c
+++ Chapter_3/Prog/prog3.3.c
@@ -13,6 +13,7 @@ int main(void)
     printf("6 + a/5*b = %i\n", 6 + a/5*b);
     printf("a/b*b = %i\n", a/b*b);
     printf("c/d*d = %f\n", c/d*d);
+    printf ("-a = %i\n", -a);
 
     return 0;
 }
